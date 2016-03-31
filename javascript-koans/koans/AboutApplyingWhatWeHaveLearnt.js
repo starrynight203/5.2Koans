390b1420bdd0fb74d93c7e883ded479877e8e6ea
Hannah Smith <starrynight203@msn.com>
2016-03-31 11:50:53 -0400

    saved unsaved file in atom

--- javascript-koans/koans/AboutApplyingWhatWeHaveLearnt.js
+++ javascript-koans/koans/AboutApplyingWhatWeHaveLearnt.js
@@ -82,13 +82,14 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(ingredientCount['mushrooms']).toBe(FILL_ME_IN);
+    expect(ingredientCount['mushrooms']).toBe(2);
   });
 
   it("should count the ingredient occurrence (functional)", function () {
     var ingredientCount = { "{ingredient name}": 0 };
 
     /* chain() together map(), flatten() and reduce() */
+    
 
     expect(ingredientCount['mushrooms']).toBe(FILL_ME_IN);
   });
